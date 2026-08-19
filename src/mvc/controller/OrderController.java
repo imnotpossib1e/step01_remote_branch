b88751e05404bdb5e351713e9cd18005664fddd2
imnotpossib1e <habit2me@gmail.com>
2026-08-19 16:09:35 +0900

    cart 브랜치에서 OrderController 수정

--- src/mvc/controller/OrderController.java
+++ src/mvc/controller/OrderController.java
@@ -2,8 +2,7 @@ package mvc.controller;
 
 public class OrderController {
     public void select(){
-        System.out.println("cart에서 수정했다");
-        System.out.println("원격에서 수정했다.");
+        System.out.printf("cart2 브랜치에서 수정했습니다.");
     }
 
 }
