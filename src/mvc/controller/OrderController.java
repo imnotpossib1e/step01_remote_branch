201348fb99e5dd09220e3cc7538a8ebdeaacb762
이진현 <habit2me@gmail.com>
2026-08-19 16:21:01 +0900

    Squash (#4)
    * squash 브랜치에서 커밋
    
    * squash 브랜치에서 커밋2
    
    * squash 브랜치에서 커밋3

--- src/mvc/controller/OrderController.java
+++ src/mvc/controller/OrderController.java
@@ -3,6 +3,9 @@ package mvc.controller;
 public class OrderController {
     public void select(){
         System.out.printf("cart2 브랜치에서 수정했습니다.");
+        System.out.println("squash를 테스트해보겠습니다.");
+        System.out.println("squash를 테스트해보겠습니다2");
+        System.out.println("squash를 테스트해보겠습니다3");
     }
 
 }
