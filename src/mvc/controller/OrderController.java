eb88736876fa1639f8b8a9b5fe4f23755c844fb7
imnotpossib1e <habit2me@gmail.com>
2026-08-19 16:19:09 +0900

    squash 브랜치에서 커밋2

--- src/mvc/controller/OrderController.java
+++ src/mvc/controller/OrderController.java
@@ -4,6 +4,7 @@ public class OrderController {
     public void select(){
         System.out.printf("cart2 브랜치에서 수정했습니다.");
         System.out.println("squash를 테스트해보겠습니다.");
+        System.out.println("squash를 테스트해보겠습니다2");
     }
 
 }
